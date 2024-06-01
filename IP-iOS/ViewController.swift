620bbe524f88416ed090b8b41b890740c01a7449
leedoyeon849 <do83430208@gmail.com>
2024-06-01 14:02:04 +0900

    setting: init project

---
+++ IP-iOS/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  IP-iOS
+//
+//  Created by DOYEON LEE on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
